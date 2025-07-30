59f261dcf093284d0636ec1bfd82d4755ab65f6a
abhijitciklum <abgi@ciklum.com>
2025-07-31 01:06:47 +0530

    aded

--- App.js
+++ App.js
@@ -1,1 +1,16 @@
-console.log("hey");
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+const structure = React.createElement("div", { id: "parent" }, [
+  React.createElement("div", { id: "child1", key: "child1" }, [
+    React.createElement("h1", { key: "h1-child1" }, "Namaste"),
+    React.createElement("h2", { key: "h2-child1" }, "React"),
+  ]),
+  React.createElement("div", { id: "child2", key: "child2" }, [
+    React.createElement("h1", { key: "h1-child2" }, "hi"),
+    React.createElement("h2", { key: "h2-child2" }, "Abhijit"),
+  ]),
+]);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(structure);
+console.log("heading", structure);
